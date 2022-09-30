4117cf0934fdd833cd889b4db11268b5095d8294
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-09-30 16:58:09 +0300

    backend: added route for placeValueInNumber function

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/sort', [handlerController::class, 'sortString']);
+
+Route::post('/place-value', [handlerController::class, 'placeValueInNumber']);
